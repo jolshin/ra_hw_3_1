5b7853b21513d7d22b60f8be43b8fd3b0b6f71dc
Николай Ёлшин <nikolajelsin@Mac-mini.local>
2025-04-11 04:15:27 +0800

    Fixed Stars.tsx and Star.tsx

--- src/components/Stars.tsx
+++ src/components/Stars.tsx
@@ -2,12 +2,20 @@ import Star from "./Star";
 import { StarsProps } from "../models/interfaces";
 
 const Stars = ({ count }: StarsProps) => {
-  return (
-    <div className="container">
-      <h2>Film's name goes here</h2>
-      <Star count={count} />
-    </div>
-  );
+  if (count > 0 && count < 6) {
+    return (
+      <div className="container">
+        <h2>Film's name goes here</h2>
+        <ul className="card-body-stars u-clearfix">
+          {new Array(count).fill("").map((_, index) => (
+            <li key={index}>
+              <Star />
+            </li>
+          ))}
+        </ul>
+      </div>
+    );
+  }
 };
 
 export default Stars;
